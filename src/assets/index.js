d40acc9c99b68485ad58975466b836adccaa9b15
Andresguc <andresguc@gmail.com>
2024-05-25 10:17:16 -0500

    feat: create the main section of the page

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,8 @@
 import sm_logo from "./sm_logo.svg";
 import close from "./close.svg";
 import menu from "./menu.svg";
+import itemMark from "./itemMark.svg";
+import arrowUp from "./arrowUp.svg";
+import ls_logo from "./ls_logo.svg";
 
-
-export { sm_logo, close, menu };
-
+export { sm_logo, close, menu, itemMark, arrowUp, ls_logo };
