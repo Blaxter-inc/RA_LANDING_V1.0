60bfff661dfee67bd574315a14aaac2bf40b2999
Andresguc <andresguc@gmail.com>
2024-05-28 07:55:17 -0500

    feat: github actions configuration 5

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.jsx"; // If you're using JSX files, keep the .jsx extension
+import App from "./App.jsx"; 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
