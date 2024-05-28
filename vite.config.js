ef18c9375a0e32fba7c08865e3ab0b9f39f89afc
Andresguc <andresguc@gmail.com>
2024-05-28 11:45:35 -0500

    feat: github actions configuration 15

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   esbuild: {
-    jsxFactory: "React.createElement", // Specify the JSX factory
-    jsxInject: `import React from 'react';`, // Inject React import for JSX
+    jsxFactory: 'React.createElement', 
+    jsxInject: `import React from 'react';`,
   },
 });
