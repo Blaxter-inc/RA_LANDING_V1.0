c2e5d406521d99b592cc2336cff0c6b601b37f03
Andresguc <andresguc@gmail.com>
2024-05-28 02:06:04 -0500

    feat: github actions configuration 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/RA_LANDING_V1.0/"
+  base: '/RA_LANDING_V1.0/'
 })
