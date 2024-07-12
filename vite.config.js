9e18ab2586d463f20a820dca1df320952c5e4377
Andresguc <andresguc@gmail.com>
2024-07-12 12:03:13 -0500

    Solving deploy error Update vite config update files to solve error

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/RA_LANDING_V1.0/", // Ensure this is the correct repository name
   esbuild: {
     jsxFactory: "React.createElement",
     jsxInject: `import React from 'react';`,
